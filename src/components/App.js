9fc2ebc048fb38fd24625be5a9e4ba851c7ef8f1
Jason Snelders <jsnelders@gmail.com>
2020-03-12 13:18:24 +1100

    Finished module 6

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React  from 'react';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
+import CoursesPage from './CoursesPage';
 import Header from './common/Header';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         const route = window.location.pathname;
 
         if (route === "/about") return <AboutPage />;
+        if (route === "/courses") return <CoursesPage />;
     
         return <HomePage />;
     }
