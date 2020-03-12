cb7ab5a5227e5216d44cc3b65eab1c326f5b34bb
Jason Snelders <jsnelders@gmail.com>
2020-03-12 13:53:41 +1100

    Finished module 7

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,52 +1,28 @@
-import React  from 'react';
+import React, { useState, useEffect }  from 'react';
 import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList";
 
-class CoursesPage extends React.Component {
 
-    state = {
-        courses: []
-    };
 
-    componentDidMount()
-    {
-        getCourses().then((courses) => {
-            this.setState({courses: courses});
+function CoursesPage() {
+
+    const [ courses, setCourses] = useState([]);
+
+    useEffect( () => {
+        getCourses().then((_courses) => {
+            setCourses(_courses);
         });
-    }
-    
-    render()
-    {
-        return (
-            <>
-                <h2>Courses</h2>
+    }, []);
 
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Author ID</th>
-                            <th>Category</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            
-                            this.state.courses.map( course => {
-                                return (
-                                    <tr key={course.id}>
-                                        <td>{course.title}</td>
-                                        <td>{course.authorId}</td>
-                                        <td>{course.category}</td>
-                                    </tr>
-                                );
-                            })
-                        }
 
-                    </tbody>
-                </table>
-            </>
-        );
-    }
+    return (
+        <>
+            <h2>Courses</h2>
+
+            <CourseList courses={courses} />
+
+        </>
+    );
 }
 
 export default CoursesPage;
